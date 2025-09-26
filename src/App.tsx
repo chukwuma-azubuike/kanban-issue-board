24869b4b49927f5dbbb023bf1d72db4e3482a311
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-26 13:31:42 +0100

    fix(undo): add toast container to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import { BoardPage } from './pages/BoardPage';
 import { IssueDetailPage } from './pages/IssueDetailPage';
 import { SettingsPage } from './pages/SettingsPage';
 import { Navigation } from './components/Navigation';
+import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
-
-  return (
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/board" element={<BoardPage />} />
-          <Route path="/issue/:id" element={<IssueDetailPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="*" element={<Navigate to="/board" />} />
-        </Routes>
-      </Router>
-  );
-}
+	return (
+		<Router>
+			<Navigation />
+			<Routes>
+				<Route path="/board" element={<BoardPage />} />
+				<Route path="/issue/:id" element={<IssueDetailPage />} />
+				<Route path="/settings" element={<SettingsPage />} />
+				<Route path="*" element={<Navigate to="/board" />} />
+			</Routes>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick={false}
+				rtl={false}
+				pauseOnFocusLoss
+				pauseOnHover
+			/>
+		</Router>
+	);
+};
